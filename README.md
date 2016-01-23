4633fab3f9562dd85f05e7792443fcac5730269e
Maximilian Voss <maximilian.voss@gmail.com>
2016-01-23 21:51:56 +0100

    doing a little clean up

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# jsontable
-Transforming JSON in a table (vice versa) for easier access and processing in C
+# Jsontable
+Transforming JSON into a hashmap for easier access and processing in C.
+
+Starting with a JSON object, looking like:
+
+```
+{  
+   "test":{  
+      "_id":{  
+         "$oid":"566950d1afc4a3c1d86fcdfb"
+      },
+      "name":"picture",
+      "file":"/var/www/html/pictureIn.png",
+      "array":[  
+         "1",
+         "b",
+         "3",
+         "d",
+         {  
+            "object":"test"
+         }
+      ],
+      "nullpointer":null,
+      "number":1234,
+      "true":true,
+      "false":false
+   }
+}
+```
+
+Will be turned into:
+```
+test._id.$oid = 566950d1afc4a3c1d86fcdfb
+test.name = picture
+test.file = /var/www/html/pictureIn.png
+test.array[0] = 1
+test.array[1] = b
+test.array[2] = 3
+test.array[3] = d
+test.array[4].object = test
+test.nullpointer = null
+test.number = 1234
+test.true = true
+test.false = false
+```
--- example.c
+++ example.c
@@ -1,12 +1,38 @@
 #include "jsontable.h"
 #include "stdio.h"
 
+
+#define JSON_EXAMPLE "{\
+   \"test\":{  \
+      \"_id\":{ \
+         \"$oid\":\"566950d1afc4a3c1d86fcdfb\"\
+      },\
+   \"name\":\"picture\",\
+   \"file\":\"/var/www/html/pictureIn.png\",\
+   \"array\":[  \
+      \"1\",\
+      \"b\",\
+      \"3\",\
+      \"d\",\
+      {  \
+         \"object\":\"test\"\
+      }\
+   ],\
+   \"nullpointer\":null,\
+   \"number\":1234,\
+   \"true\":true,\
+   \"false\":false\
+	}\
+}"
+
+
 void hookMethod(void *data, char *key, char *value) {
 	printf("%s = %s\n", key, value);
 }
 
 int main(int argc, char **argv) {
+	printf("%s\n", JSON_EXAMPLE);
 	jsontable_registerHook(NULL, &hookMethod);
-	jsontable_parse("{ \"test\": { \"_id\" : { \"$oid\" : \"566950d1afc4a3c1d86fcdfb\" } }, \"name\" : \"picture\", \"file\" : \"/var/www/html/pictureIn.png\", \"moep\": [\"a\", \"b\", \"c\", \"d\", { \"test\": \"blubb\"}], \"bla\": null, \"number\": 1234, \"true\": true, \"false\": false }");
+	jsontable_parse(JSON_EXAMPLE);
 	return 0;
 }
