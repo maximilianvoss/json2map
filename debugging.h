f9b2f1846080afd68297ec26a8ef07b275fd44f4
Maximilian Voss <maximilian.voss@gmail.com>
2016-05-03 23:24:51 +0200

    fixing debugging directive

--- debugging.h
+++ debugging.h
@@ -2,9 +2,11 @@
 #define __DEBUGGING_H__
 
 #ifdef DEBUG
-#define DEBUG_TEXT(fmt, ...) printf(fmt, __VA_ARGS__)
-#define DEBUG_PUT(fmt) printf(fmt)
+#define DEBUG_TEXT(fmt, ...) printf(fmt, __VA_ARGS__); printf("\n")
+#define DEBUG_PUT(fmt) printf(fmt); printf("\n")
 #else
 #define DEBUG_TEXT(fmt, ...)
 #define DEBUG_PUT(fmt)
+#endif
+
 #endif
