02982ceb93f9f9696f8a7d4bc948b50e273a8e36
Maximilian Voss <maximilian.voss@gmail.com>
2017-06-07 12:52:27 +0200

    optimize logging

--- logging.h
+++ logging.h
@@ -12,10 +12,10 @@
 #define LOGGING_LEVEL 0
 #endif
 
-#define LOGGING_ERROR(fmt, ...) { fprintf(stderr, "ERROR: %s: ", __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__); fprintf(stderr,"\n"); }
-#define LOGGING_WARN(fmt, ...) if ( LOGGING_LEVEL > 0 ) { fprintf(stderr, "WARN: %s: ", __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__); fprintf(stderr, "\n"); }
-#define LOGGING_INFO(fmt, ...) if ( LOGGING_LEVEL > 1 ) { fprintf(stderr, "INFO: %s: ", __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__); fprintf(stderr, "\n"); }
-#define LOGGING_DEBUG(fmt, ...) if ( LOGGING_LEVEL > 2 ) { fprintf(stderr, "DEBUG: %s: ", __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__); fprintf(stderr, "\n"); }
-#define LOGGING_TRACE(fmt, ...) if ( LOGGING_LEVEL > 3 ) { fprintf(stderr, "TRACE: %s: ", __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__); fprintf(stderr, "\n"); }
+#define LOGGING_ERROR(fmt, ...) { fprintf(stderr, "ERROR: %s:%d - %s: ", __FILE__, __LINE__, __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__); fprintf(stderr,"\n"); }
+#define LOGGING_WARN(fmt, ...) if ( LOGGING_LEVEL > 0 ) { fprintf(stderr, "WARN: %s:%d - %s: ", __FILE__, __LINE__, __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__); fprintf(stderr,"\n"); }
+#define LOGGING_INFO(fmt, ...) if ( LOGGING_LEVEL > 1 ) { fprintf(stderr, "INFO: %s:%d - %s: ", __FILE__, __LINE__, __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__); fprintf(stderr,"\n"); }
+#define LOGGING_DEBUG(fmt, ...) if ( LOGGING_LEVEL > 2 ) { fprintf(stderr, "DEBUG: %s:%d - %s: ", __FILE__, __LINE__, __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__); fprintf(stderr, "\n"); }
+#define LOGGING_TRACE(fmt, ...) if ( LOGGING_LEVEL > 3 ) { fprintf(stderr, "TRACE: %s:%d - %s: ", __FILE__, __LINE__, __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__); fprintf(stderr, "\n"); }
 
 #endif
